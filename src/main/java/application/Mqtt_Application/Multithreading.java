0020436a26af84a5ee8012c3261993051659fff4
trungdinh98 <batrungdinh33@gmail.com>
2020-11-18 10:11:40 +0700

    Update

--- src/main/java/application/Mqtt_Application/Multithreading.java
+++ src/main/java/application/Mqtt_Application/Multithreading.java
@@ -18,10 +18,10 @@ public class Multithreading extends Thread {
 
     public static final String set_topic = "house-";
     private MqttClient client;
-    public static final String BROKER_URL = "tcp://10.10.1.146:1883";
+    public static final String BROKER_URL = "tcp://localhost:1883";
     CountDownLatch latch;
     static int qos = 1;
-    static String path = "/root/mqtt_data"; // Change your path
+    static String path = "/home/ubuntu/mqtt_data"; // Change your path
 
     public Multithreading (CountDownLatch latch) {
         super();
